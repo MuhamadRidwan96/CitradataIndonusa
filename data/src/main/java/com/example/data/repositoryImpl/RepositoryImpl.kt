3b24cf1edfa5b0a7145b24cd51e2bd8d53b3c7fb
MhmdRdn <m.rdn.96@gmail.com>
2025-03-21 18:01:03 +0700

    menambahakan register function di repository

--- data/src/main/java/com/example/data/repositoryImpl/RepositoryImpl.kt
+++ data/src/main/java/com/example/data/repositoryImpl/RepositoryImpl.kt
@@ -4,8 +4,10 @@ import com.example.data.remote.ApiHelper
 import com.example.data.utils.Constant
 import com.example.data.utils.ErrorHandle
 import com.example.domain.model.LoginModel
+import com.example.domain.model.RegisterModel
 import com.example.domain.repository.Repository
 import com.example.domain.response.LoginResponse
+import com.example.domain.response.RegisterResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -27,7 +29,28 @@ class RepositoryImpl @Inject constructor(private val apiHelper: ApiHelper) : Rep
             val errorBody = response.errorBody()?.string()
             val errorMessage = try {
                 errorBody?.let { ErrorHandle.parseErrorBody(it) } ?: Constant.UNKNOWN_ERROR
-            } catch (e:Exception){
+            } catch (e: Exception) {
+                Constant.FAILED_PARSE
+            }
+            emit(Result.failure(Exception(errorMessage)))
+        }
+    }
+
+    override fun register(requestRegister: RegisterModel): Flow<Result<RegisterResponse>> = flow {
+        val response = try {
+            apiHelper.register(requestRegister)
+        } catch (e: Exception) {
+            emit(Result.failure(Exception("Network Error: ${e.localizedMessage}")))
+            return@flow
+        }
+        if (response.isSuccessful) {
+            response.body()?.let { emit(Result.success(it)) } ?: emit(
+                Result.failure(Exception(Constant.ERROR_NULL)))
+        } else {
+            val errorBody = response.errorBody()?.string()
+            val errorMessage = try {
+                errorBody?.let { ErrorHandle.parseErrorBody(it) } ?: Constant.UNKNOWN_ERROR
+            } catch (e: Exception) {
                 Constant.FAILED_PARSE
             }
             emit(Result.failure(Exception(errorMessage)))
--- domain/src/main/java/com/example/domain/repository/Repository.kt
+++ domain/src/main/java/com/example/domain/repository/Repository.kt
@@ -2,9 +2,12 @@ package com.example.domain.repository
 
 
 import com.example.domain.model.LoginModel
+import com.example.domain.model.RegisterModel
 import com.example.domain.response.LoginResponse
+import com.example.domain.response.RegisterResponse
 import kotlinx.coroutines.flow.Flow
 
 interface Repository {
    fun login(requestLogin: LoginModel): Flow<Result<LoginResponse>>
+   fun register(requestRegister:RegisterModel):Flow<Result<RegisterResponse>>
 }
