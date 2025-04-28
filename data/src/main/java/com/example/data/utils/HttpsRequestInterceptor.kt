9ca78b48821aacdbb685c4b9acf0e34eda8faec1
MhmdRdn <m.rdn.96@gmail.com>
2025-04-28 09:10:07 +0700

    Refactor NetworkModule: replace AuthInterceptor with HttpsRequestInterceptor

---
+++ data/src/main/java/com/example/data/utils/HttpsRequestInterceptor.kt
@@ -0,0 +1,20 @@
+package com.example.data.utils
+
+import com.example.data.preferencesImpl.UserPreferencesImpl
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
+import okhttp3.Response
+import javax.inject.Inject
+
+class HttpsRequestInterceptor @Inject constructor(private val userPreferenceImpl: UserPreferencesImpl) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val token = runBlocking {
+           userPreferenceImpl.getSession().firstOrNull()?.token
+        } ?:throw IllegalArgumentException("Token is not available. Please login first.")
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization","Bearer $token")
+            .build()
+        return chain.proceed(request)
+    }
+}
