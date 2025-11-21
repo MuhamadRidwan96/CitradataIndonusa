a285493c386206c19ba888d6b771c6400cb8edd3
MhmdRdn <m.rdn.96@gmail.com>
2025-11-21 10:07:53 +0700

    refactor : update data module and profile repositoryimpl

--- data/src/main/java/com/example/data/repositoryImpl/ProfileRepositoryImpl.kt
+++ data/src/main/java/com/example/data/repositoryImpl/ProfileRepositoryImpl.kt
@@ -1,4 +1,17 @@
 package com.example.data.repositoryImpl
 
-class ProfileRepositoryImpl {
+import com.example.common.Result
+import com.example.data.remote.api.ApiHelper
+import com.example.data.utils.toResult
+import com.example.domain.repository.ProfileRepository
+import com.example.domain.response.ProfileResponse
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class ProfileRepositoryImpl @Inject constructor(private val apiHelper: ApiHelper) : ProfileRepository {
+    override suspend fun getUser(): Flow<Result<ProfileResponse>> = flow {
+        val response = apiHelper.getUser()
+        emit(response.toResult())
+    }
 }
